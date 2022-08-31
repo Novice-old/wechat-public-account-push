e61c2ee413a7baaf07904604f4f9ace4d02c3c1f
Novice-old <112534264+Novice-old@users.noreply.github.com>
2022-08-31 16:25:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     APP_SECRET: "51f7392bb0bb7c1c977f7dd81073163a",
 
     // 模板消息id
-    TEMPLATE_ID: "SNRfhmSgS9KJ7p1fJF09gOcaNh7tuoNhXED6eLMAdXU",
+    TEMPLATE_ID: "3_lPrAgl-AgXGdRo1aaAuDoJBQ00YMQGTf9RU_IZS4E",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
 
@@ -50,7 +50,7 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "测试", "year": "1996", "date": "01-01"},
+      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
       {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
@@ -66,15 +66,13 @@ export const config = {
     LOVE_DATE: "2015-05-01",
     // 结婚纪念日
     MARRY_DATE: "2020-01-04",
-    //生日倒计时
-    birthday_message.DATA:"2022-09-03
 
 
     /** 每日一言 */
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: "动画"
+    LITERARY_PREFERENCE: ""
 
 
     }
@@ -86,7 +84,7 @@ export const config = {
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
 // 今天是我们结婚的第{{marry_day.DATA}}天
-// 生日还有{{birthday_message.DATA}}天
+// {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
